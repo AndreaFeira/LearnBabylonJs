e304a7316103d459d0e92bcf58ac05bac0bb8bfd
tss <tss@debian-tss24>
2016-07-05 08:59:45 +0200

    lista asta OK

--- src/main/java/it/feira/presentation/Index.java
+++ src/main/java/it/feira/presentation/Index.java
@@ -1,6 +1,11 @@
 package it.feira.presentation;
 
-import javax.ejb.Stateless;
+import it.feira.business.boundary.AstaFacade;
+import it.feira.business.entity.Asta;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -8,9 +13,25 @@ import javax.inject.Named;
  * @author Andrea Feira
  */
 @Named(value = "index")
-@Stateless
+@RequestScoped
 public class Index {
-    
-    
+
+    private List<Asta> asteInCorso;
+
+    @Inject
+    AstaFacade astaSrv;
+
+    public List<Asta> getAsteInCorso() {
+        return asteInCorso;
+    }
+
+    public void setAsteInCorso(List<Asta> asteInCorso) {
+        this.asteInCorso = asteInCorso;
+    }
+
+    @PostConstruct
+    public void initAste() {
+        asteInCorso = astaSrv.auctionInProgress();
+    }
 
 }
